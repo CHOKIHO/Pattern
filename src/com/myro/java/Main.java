316a13d5b499b0cd24d766171f2415c366c78274
banff <banfff@kg21.net>
2016-10-14 18:46:02 +0900

    브릿지 패턴을 안쓸경우

---
+++ src/com/myro/java/Main.java
@@ -0,0 +1,14 @@
+package com.myro.java;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		printMorseCode print = new printMorseCode();
+		
+		print.a();
+		print.b();
+		print.c();
+	}
+
+}
---
+++ src/com/myro/java/printMorseCode.java
@@ -0,0 +1,19 @@
+package com.myro.java;
+
+public class printMorseCode extends MorseCode {
+	
+	//print a
+	public void a() {
+		dash();dot();dash();space();
+	}
+	
+	//print b
+	public void b() {
+		dot();dash();dot();space();
+	}
+
+	//print c
+	public void c() {
+		dot();dash();dash();space();
+	}
+}
